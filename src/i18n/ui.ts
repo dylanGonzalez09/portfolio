82ff3759d0e533a95b2576d324d07d2c66c37c22
Dylan Gonzalez <dyg09032003@gmail.com>
2025-06-22 00:24:45 -0500

    add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,33 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "nav.experience": "Experiencia",
+    "nav.projects": "Proyectos",
+    "nav.stack": "Stack",
+    "nav.contact": "Contacto",
+    "profile.name": "Hola soy Dylan",
+    "profile.post": "Desarrollador de Software",
+    "profile.description":
+      "Cuento con +3 años de experiencia desarrollando software. He tenido la oportunidad de trabajar en un proyecto profesional creando soluciones modernas, eficientes y escalables, combinando un sólido conocimiento en tecnologías frontend y backend con para entregar productos de alta calidad.",
+    "profile.contact": "Contactame",
+    "profile.cv": "CV",
+  },
+  en: {
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
+    "nav.stack": "Stack",
+    "nav.contact": "Contact",
+    "profile.name": "Hi, I'm Dylan",
+    "profile.post": "Software Developer",
+    "profile.description":
+      "I have over +3 years of experience developing software. I've had the opportunity to work on a professional project, creating modern, efficient, and scalable solutions, combining a solid understanding of front- and back-end technologies to deliver high-quality products.",
+    "profile.contact": "Contact me",
+    "profile.cv": "CV",
+  },
+} as const;
